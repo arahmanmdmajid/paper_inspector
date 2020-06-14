75ad93da5a0294786e005cbc9e41477f57627889
arahman <arahman1986@gmail.com>
2020-06-14 18:02:16 +0300

    new file with citation code

---
+++ getCitatiton.py
@@ -0,0 +1,7 @@
+import json
+from serpapi.google_scholar_search_results import GoogleScholarSearchResults
+client = GoogleScholarSearchResults({"q": "Coffee"})
+data = client.get_json()
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
